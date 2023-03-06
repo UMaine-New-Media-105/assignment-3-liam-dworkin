1538833cd67db2ee4ef5fe2b4ddc7f319a30f009
liam-dworkin <124173727+liam-dworkin@users.noreply.github.com>
2023-03-06 16:30:47 -0500

    Update dworkinA3C2.js
    Combining the separate functions for 'yeetBall' and 'yeetBall2' so that they include the conditional for the mouse in a single function.

--- dworkinA3C2.js
+++ dworkinA3C2.js
@@ -9,17 +9,11 @@ function setup() {
 
 function draw() {
   background("220");
-
   push();
-  if (mouseIsPressed) {
-    //let x2 = constrain(mouseX, leftW, rightW);
-    yeetBall2(mouseX, 420, 1);
-  } else {
-    //let x2 = constrain(mouseX, leftW, rightW);
-    yeetBall(mouseX, 430, 1);
-  }
-
-  //scaled up 1:1.5 for reference at a larger size
+  //let x2 = constrain(mouseX, leftW, rightW);
+  //Trying to define boundaries haven't finished it yet.
+  yeetBall(mouseX, 420, 1);
+  pop();
 }
 
 //I wanna make my lil sprite into my game character. I plan on adding in secondary animations that change the face text/font/size so that I can do a few different animations without actually having to do a ton of visual aid and coding. In one of my smaller functions for an exit ticket I added in a lil face changing aspect in the font functions.
@@ -28,50 +22,52 @@ function draw() {
 
 //I made a function and called it back- I might add a slider for gradience in HSLA if I can to be called back in the function in 'draw'
 
-function yeetBall(x, y, size) {
-  push();
-  translate(x, y);
-  scale(size);
-  noStroke();
-  translate(0, 0);
-  fill("hsla(50, 45%, 75%, .7)");
-  ellipse(0, 0, 91, 96);
-  fill("hsla(100, 45%, 75%, .7)");
-  ellipse(0, 0, 80, 85);
-  fill("hsla(230, 45%, 75%, .7)");
-  ellipse(0, 0, 63, 64);
-  fill("hsla(230, 45%, 75%, .7)");
-  ellipse(0, 0, 45, 47);
-  fill("purple");
-  textSize(25);
-  noStroke();
-  text("( ^-^ )", -31, -4);
-  fill("salmon");
-  textSize(20);
-  text("◍   ◍", -25, 0);
-  pop();
-}
+//I've added both functions for yeetBall together and added the conditional to the function so it can be applied easily by simply calling the function back which includes the mouseIsPressed conditional in it.
 
-function yeetBall2(x, y, size) {
-  push();
-  translate(x, y);
-  scale(size);
-  noStroke();
-  translate(0, 0);
-  fill("hsla(50, 45%, 75%, .7)");
-  ellipse(0, 0, 101, 75);
-  fill("hsla(100, 45%, 75%, .7)");
-  ellipse(0, 0, 95, 67);
-  fill("hsla(230, 45%, 75%, .7)");
-  ellipse(0, 0, 75, 47);
-  fill("hsla(230, 45%, 75%, .7)");
-  ellipse(0, 0, 57, 39);
-  fill("purple");
-  textSize(25);
-  noStroke();
-  text("_ >o< _", -41, 2);
-  fill("salmon");
-  textSize(20);
-  text("◍   ◍", -25, 9);
-  pop();
+function yeetBall(x, y, size) {
+  if (mouseIsPressed) {
+    push();
+    translate(x, y);
+    scale(size);
+    noStroke();
+    translate(0, 0);
+    fill("hsla(50, 45%, 75%, .7)");
+    ellipse(0, 0, 101, 75);
+    fill("hsla(100, 45%, 75%, .7)");
+    ellipse(0, 0, 95, 67);
+    fill("hsla(230, 45%, 75%, .7)");
+    ellipse(0, 0, 75, 47);
+    fill("hsla(230, 45%, 75%, .7)");
+    ellipse(0, 0, 57, 39);
+    fill("purple");
+    textSize(25);
+    noStroke();
+    text("_ >o< _", -41, 2);
+    fill("salmon");
+    textSize(20);
+    text("◍    ◍", -27, 6);
+    pop();
+  } else {
+    push();
+    translate(x, y);
+    scale(size);
+    noStroke();
+    translate(0, 0);
+    fill("hsla(50, 45%, 75%, .7)");
+    ellipse(0, 0, 91, 96);
+    fill("hsla(100, 45%, 75%, .7)");
+    ellipse(0, 0, 80, 85);
+    fill("hsla(230, 45%, 75%, .7)");
+    ellipse(0, 0, 63, 64);
+    fill("hsla(230, 45%, 75%, .7)");
+    ellipse(0, 0, 45, 47);
+    fill("purple");
+    textSize(25);
+    noStroke();
+    text("( ^-^ )", -31, -4);
+    fill("salmon");
+    textSize(20);
+    text("◍   ◍", -25, 0);
+    pop();
+  }
 }
